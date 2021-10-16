ae73a1497cd92ae0d3c7942fc05291bb189f62a1
121291 <bamarxd@gmail.com>
2021-10-16 08:42:10 +0100

    hi

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
 echo off
 git add --all
-git commit -m YO WASSUP"
+git commit -m "hi"
 cls
 echo Added and committed in 1.3 seconds.
 echo Initiating push request..
--- push.bat
+++ push.bat
@@ -1,6 +1,5 @@
 cls
 echo Pushing...
-git pull
 git push
 echo Pushed to repo.
 pause
